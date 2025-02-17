c46a8d4b869bb24ba3462aca12dd49bd09a3b74f
Jonathanpv99 <jonyvegapjv@gmail.com>
2025-02-17 11:57:04 -0600

    feat: add intent to phone

--- app/src/main/java/com/example/intentsapp/MainActivity.kt
+++ app/src/main/java/com/example/intentsapp/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,7 +23,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntentsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeView()
+                    Surface (modifier = Modifier.fillMaxSize().systemBarsPadding()) {
+                        HomeView()
+                    }
                 }
             }
         }
